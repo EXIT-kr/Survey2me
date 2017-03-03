6b7af3fd945cf4c2b7ffe07fe6b2f7a4975c8026
nayunhwan <nayunhwan.dev@gmail.com>
2017-03-03 16:00:03 +0900

    [v0.0.1] Add React-Bootstrap

--- src/Pages/DefaultPage/DefaultPage.js
+++ src/Pages/DefaultPage/DefaultPage.js
@@ -1,8 +1,8 @@
-
 // This Page is Skeleton of React Structure for Web Development
 // If you want to make other page, Copy and Refactor this page.
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 
 class DefaultPage extends Component {
   constructor(props) {
@@ -13,6 +13,11 @@ class DefaultPage extends Component {
     return (
       <div>
         This is Default Page
+        <ButtonToolbar>
+          <Button bsStyle="primary" bsSize="large">Large button</Button>
+          <Button bsSize="large">Large button</Button>
+        </ButtonToolbar>
+
       </div>
     );
   }
