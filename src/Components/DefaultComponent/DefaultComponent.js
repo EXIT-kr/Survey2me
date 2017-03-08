ec735e76faff45d2992e0c17332cc45cd2648d2d
nayunhwan <nayunhwan.dev@gmail.com>
2017-03-08 22:16:05 +0900

    [v0.0.1] Add Default Component

---
+++ src/Components/DefaultComponent/DefaultComponent.js
@@ -0,0 +1,27 @@
+// This Page is Skeleton of React Structure for Web Development
+// If you want to make other page, Copy and Refactor this page.
+
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react'
+
+// Setting for Props
+const defaultProps = {};
+const propTypes = {};
+
+class DefaultComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {} = this.props;
+    return (
+      <div></div>
+    );
+  }
+}
+
+DefaultComponent.defaultProps = defaultProps;
+DefaultComponent.propTypes = propTypes;
+
+export default DefaultComponent
